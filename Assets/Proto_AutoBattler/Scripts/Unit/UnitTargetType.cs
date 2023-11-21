bcf0d99f76c78bc3217ff02584e5a7857cec1f91
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2023-11-20 20:09:08 -0500

    Added unitTypes SO and a UnitController script

---
+++ Assets/Proto_AutoBattler/Scripts/Unit/UnitTargetType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class UnitTargetType : ScriptableObject
+{
+    public List<UnitTargetType> TypesToTarget;
+}
